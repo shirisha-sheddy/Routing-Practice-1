606411ff761938ded353554ada860985727be220
shirisha-sheddy <shirishasheddy963@gmail.com>
2023-10-18 06:10:56 +0000

    Routing Practice 1

---
+++ src/components/About/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+
+import './index.css'
+
+const About = () => (
+  <div>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt="about"
+    />
+    <h1>About</h1>
+    <p>I love to create! I am a frontend web developer</p>
+  </div>
+)
+
+export default About
